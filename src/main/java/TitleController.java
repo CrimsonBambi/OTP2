cca3b51710ab15971db7f8d2b0240bc0e9830d77
CrimsonBambi <gerli.hirv@metropolia.fi>
2025-11-25 14:52:02 +0200

    initial commit

---
+++ src/main/java/TitleController.java
@@ -0,0 +1,8 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+
+public class TitleController {
+    @FXML Label titleLabel;
+    @FXML ChoiceBox<String> languages;
+}
